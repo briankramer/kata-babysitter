cfdb7ddf403b20685576f01b26aac3bd14775634
Brian Kramer <bkramer@tutorgen.com>
2019-03-25 12:44:53 -0400

    Ensure AM time strings work as well

--- test_calculate_charge.py
+++ test_calculate_charge.py
@@ -3,7 +3,11 @@ import unittest
 import calculate_charge as calc
 
 class CalculateCharge(unittest.TestCase):
-    def test_when_convert_string_to_time_is_passed_valid_string_it_returns_correct_time(self):
+    def test_when_convert_string_to_time_is_passed_valid_am_string_it_returns_correct_time(self):
+        time = calc.convert_string_to_time('4:23AM')
+        self.assertEqual(time, datetime(year=1900, month=1, day=1, hour=4, minute=23).time())
+
+    def test_when_convert_string_to_time_is_passed_valid_pm_string_it_returns_correct_time(self):
         time = calc.convert_string_to_time('5:01PM')
         self.assertEqual(time, datetime(year=1900, month=1, day=1, hour=17, minute=1).time())
 
