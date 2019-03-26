e2a7da185eed791f98ec23334ca8c4b9c59c0db9
Brian Kramer <bkramer@tutorgen.com>
2019-03-26 16:40:41 -0400

    Add test for pm start, am end, pm cutoff

--- test_calculate_charge.py
+++ test_calculate_charge.py
@@ -48,3 +48,6 @@ class CalculateCharge(unittest.TestCase):
 
     def test_when_calc_hours_passed_am_end_am_start_am_cutoff_it_returns_hours(self):
         self.assertEqual(calc.calc_hours(get_time(1), get_time(4), get_time(3)), 2)
+
+    def test_when_calc_hours_passed_pm_start_am_end_pm_cutoff_it_returns_hours(self):
+        self.assertEqual(calc.calc_hours(get_time(17), get_time(3), get_time(19)), 2)
