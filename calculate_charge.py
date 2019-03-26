213bf942cf76de5bda6ddbd6e145a5365cb35f7b
Brian Kramer <bkramer@tutorgen.com>
2019-03-26 15:48:13 -0400

    Begin to calculate sitting hours

--- calculate_charge.py
+++ calculate_charge.py
@@ -1,5 +1,21 @@
 from datetime import datetime
 
+family_a = [
+    ('5:00PM', 15),
+    ('11:00PM', 20)
+]
+
+family_b = [
+    ('5:00PM', 12),
+    ('10:00PM', 8),
+    ('12:00AM', 16),
+]
+
+family_c = [
+    ('5:00PM', 21),
+    ('9:00PM', 15),
+]
+
 def convert_string_to_time(time_str):
     '''Convert string to time so that we can keep format laid out in GitHub: 5:00PM'''
     try:
@@ -16,3 +32,6 @@ def is_start_time_before_end_time(start_time, end_time):
     if start_time.hour >= 12 and end_time.hour < 12:
         return True
     return end_time >= start_time
+
+def calculate_sitting_hours_before_time(start_time, end_time, cutoff_time):
+    return 0
